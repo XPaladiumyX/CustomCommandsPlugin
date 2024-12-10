728b515fc1f7b67ada8d1bb8dc3c4bb1ff1f243b
XPaladiumyX <allegra.yann@gmail.com>
2024-12-10 21:56:52 +0100

    Added Startup and close message + Translations

--- src/main/java/skyxnetwork/customCommandsPlugin/CustomCommandsPlugin.java
+++ src/main/java/skyxnetwork/customCommandsPlugin/CustomCommandsPlugin.java
@@ -1,5 +1,6 @@
 package skyxnetwork.customCommandsPlugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import skyxnetwork.customCommandsPlugin.commands.Craft;
 import skyxnetwork.customCommandsPlugin.commands.Enderchest;
@@ -7,45 +8,71 @@ import skyxnetwork.customCommandsPlugin.commands.ReloadCommand;
 import skyxnetwork.customCommandsPlugin.commands.Smelt;
 
 public class CustomCommandsPlugin extends JavaPlugin {
+    private static final String ANSI_MAGENTA = "\u001B[35m";
+    private static final String ANSI_LIGHT_GRAY = "\u001B[37m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_LIGHT_GREEN = "\u001B[92m";
+    private static final String ANSI_RED = "\u001B[31m";
 
     @Override
     public void onEnable() {
+        Bukkit.getLogger().info(ANSI_LIGHT_GRAY + "︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹");
+        Bukkit.getLogger().info(ANSI_MAGENTA + " _______  ___   _  __   __    __   __    __    _  _______  _______ " + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "|       ||   | | ||  | |  |  |  |_|  |  |  |  | ||       ||       |" + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "|  _____||   |_| ||  |_|  |  |       |  |   |_| ||    ___||_     _|" + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "| |_____ |      _||       |  |       |  |       ||   |___   |   |  " + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "|_____  ||     |_ |_     _|   |     |   |  _    ||    ___|  |   |  " + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + " _____| ||    _  |  |   |    |   _   |  | | |   ||   |___   |   |  " + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "|_______||___| |_|  |___|    |__| |__|  |_|  |__||_______|  |___|  " + ANSI_RESET);
+        Bukkit.getLogger().info("   ");
+        Bukkit.getLogger().info(ANSI_LIGHT_GREEN + "Plugin CustomCommandsPlugin enabled !");
+        Bukkit.getLogger().info(ANSI_LIGHT_GRAY + "︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺");
+
         saveDefaultConfig(); // Charger ou créer config.yml
 
         // Enregistrer les commandes
         if (getCommand("craft") != null) {
             getCommand("craft").setExecutor(new Craft());
         } else {
-            getLogger().warning("La commande /craft n'est pas définie dans plugin.yml !");
+            getLogger().warning("/craft command is not defined in plugin.yml!");
         }
 
         if (getCommand("ec") != null) {
             getCommand("ec").setExecutor(new Enderchest());
         } else {
-            getLogger().warning("La commande /ec n'est pas définie dans plugin.yml !");
+            getLogger().warning("/ec command is not defined in plugin.yml!");
         }
 
         if (getCommand("enderchest") != null) {
             getCommand("enderchest").setExecutor(new Enderchest());
         } else {
-            getLogger().warning("La commande /enderchest n'est pas définie dans plugin.yml !");
+            getLogger().warning("/enderchest command is not defined in plugin.yml!");
         }
 
         if (getCommand("smelt") != null) {
             getCommand("smelt").setExecutor(new Smelt(this));
         } else {
-            getLogger().warning("La commande /smelt n'est pas définie dans plugin.yml !");
+            getLogger().warning("/smelt command is not defined in plugin.yml!");
         }
         if (getCommand("customcommandsplugin") != null) {
             getCommand("customcommandsplugin").setExecutor(new ReloadCommand(this));
         } else {
-            getLogger().warning("La commande /CustomCommandsPlugin n'est pas définie dans plugin.yml !");
+            getLogger().warning("The /CustomCommandsPlugin command is not defined in plugin.yml!");
         }
-        getLogger().info("CustomCommandsPlugin activé !");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("CustomCommandsPlugin désactivé !");
+        Bukkit.getLogger().info(ANSI_LIGHT_GRAY + "︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹︹");
+        Bukkit.getLogger().info(ANSI_MAGENTA + " _______  ___   _  __   __    __   __    __    _  _______  _______ " + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "|       ||   | | ||  | |  |  |  |_|  |  |  |  | ||       ||       |" + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "|  _____||   |_| ||  |_|  |  |       |  |   |_| ||    ___||_     _|" + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "| |_____ |      _||       |  |       |  |       ||   |___   |   |  " + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "|_____  ||     |_ |_     _|   |     |   |  _    ||    ___|  |   |  " + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + " _____| ||    _  |  |   |    |   _   |  | | |   ||   |___   |   |  " + ANSI_RESET);
+        Bukkit.getLogger().info(ANSI_MAGENTA + "|_______||___| |_|  |___|    |__| |__|  |_|  |__||_______|  |___|  " + ANSI_RESET);
+        Bukkit.getLogger().info("   ");
+        Bukkit.getLogger().info(ANSI_RED + "  Plugin CustomCommandsPlugin disabled !");
+        Bukkit.getLogger().info(ANSI_LIGHT_GRAY + "︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺︺");
     }
 }
