8fd8c6652deb7daaf3310298599e51b88ccfab6f
XPaladiumyX <allegra.yann@gmail.com>
2024-12-10 21:25:00 +0100

    /Craft Code

---
+++ src/main/java/skyxnetwork/customCommandsPlugin/commands/Craft.java
@@ -0,0 +1,19 @@
+package skyxnetwork.customCommandsPlugin.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Craft implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player player) {
+            player.openWorkbench(null, true); // Ouvre une table de craft virtuelle
+            return true;
+        }
+        sender.sendMessage("Cette commande est uniquement pour les joueurs !");
+        return false;
+    }
+}
