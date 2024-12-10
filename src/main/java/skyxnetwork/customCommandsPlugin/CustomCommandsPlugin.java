9153906878749251117f612fa460ddb98e1e8af7
XPaladiumyX <allegra.yann@gmail.com>
2024-12-10 21:25:40 +0100

    Main Code

--- src/main/java/skyxnetwork/customCommandsPlugin/CustomCommandsPlugin.java
+++ src/main/java/skyxnetwork/customCommandsPlugin/CustomCommandsPlugin.java
@@ -1,17 +1,46 @@
 package skyxnetwork.customCommandsPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import skyxnetwork.customCommandsPlugin.commands.Craft;
+import skyxnetwork.customCommandsPlugin.commands.Enderchest;
+import skyxnetwork.customCommandsPlugin.commands.Smelt;
 
-public final class CustomCommandsPlugin extends JavaPlugin {
+public class CustomCommandsPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig(); // Charger ou créer config.yml
 
+        // Enregistrer les commandes
+        if (getCommand("craft") != null) {
+            getCommand("craft").setExecutor(new Craft());
+        } else {
+            getLogger().warning("La commande /craft n'est pas définie dans plugin.yml !");
+        }
+
+        if (getCommand("ec") != null) {
+            getCommand("ec").setExecutor(new Enderchest());
+        } else {
+            getLogger().warning("La commande /ec n'est pas définie dans plugin.yml !");
+        }
+
+        if (getCommand("enderchest") != null) {
+            getCommand("enderchest").setExecutor(new Enderchest());
+        } else {
+            getLogger().warning("La commande /enderchest n'est pas définie dans plugin.yml !");
+        }
+
+        if (getCommand("smelt") != null) {
+            getCommand("smelt").setExecutor(new Smelt(this));
+        } else {
+            getLogger().warning("La commande /smelt n'est pas définie dans plugin.yml !");
+        }
+
+        getLogger().info("CustomCommandsPlugin activé !");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("CustomCommandsPlugin désactivé !");
     }
 }
